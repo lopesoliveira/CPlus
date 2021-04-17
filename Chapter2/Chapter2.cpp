d4bce6052cac20aa3318e49341062621dad44619
JoaoCTW <joao.oliveira@criticaltechworks.com>
2021-04-18 00:35:44 +0100

    end chapter

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -2,12 +2,59 @@
 //
 
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
 
 int main()
 {
     std::cout << "Hello World!\n";
     std::cout << 42 << std::endl;
     std::cout << 2 + 2 << std::endl;
+    std::cout << 2.2 << std::endl;
+    std::cout << 4 + 2.2 + 3 + 0.8 << std::endl;
+
+    auto x = "joao";
+    std::cout << x << std::endl;
+
+    int n = 0;
+    cout << "Enter a number:";
+    cin >> n;
+
+    cout << "Hoje dia 16 de Abril de 2010 o Sprting ganhou 1-0 ao Farense. Falta pouco para o título!" << endl;
+
+    /* try {
+         if ((int)n)
+         {
+             cout << "The number you entered is ";
+             cout << n << endl;
+         }
+         else
+         {
+             throw(n);
+         }
+     }
+     catch (string y)
+     {
+         cout << "You did not enter a numer";
+     }*/
+
+
+     /* try {
+          int age = 18;
+          if (age >= 18) {
+              cout << "Access granted - you are old enough.";
+          }
+          else {
+              throw (age);
+          }
+      }
+      catch (int myNum) {
+          cout << "Access denied - You must be at least 18 years old.\n";
+          cout << "Age is: " << myNum;*/
+          //}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
